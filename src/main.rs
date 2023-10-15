234a822c4b44b6784787b2fb993feb849c65d671
RodrigoS1546 <rodrigo.santos@adsglobal.pt>
2023-10-15 16:05:03 +0100

    Added argument options

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
+use std::num::IntErrorKind;
+
 mod parser;
 mod tokenizer;
 
 fn main() {
     let mut args = std::env::args().skip(1);
-    let mut output_file = String::from("a.out");
+    let mut output_file: Option<String> = None;
     let mut input_file: Option<String> = None;
+    let mut interpret = false;
 
     while let Some(arg) = args.next() {
         if arg.as_str() == "-o" {
-            output_file = args.next().unwrap_or_else(|| {
+            output_file = Some(args.next().unwrap_or_else(|| {
                 eprintln!("Erro: Falta o ficheiro de saída depois do -o.");
                 std::process::exit(1);
-            });
+            }));
+        } if arg.as_str() == "-i" {
+            interpret = true;
         } else if let Some(_) = input_file {
             eprintln!("Erro: O compilador só aceita um ficheiro de entrada.");
             std::process::exit(1);
         } else {
             input_file = Some(arg);
         }
+    }
+
+    if output_file.is_none() && !interpret {
+        eprintln!("Erro: Tens de escolher entre interpretar e compilar!");
+        std::process::exit(1);
     }
 
     let input_file = input_file.unwrap_or_else(|| {
@@ -40,5 +50,5 @@ fn main() {
         std::process::exit(1);
     });
 
-    dbg!(output_file, program);
+    dbg!(output_file, interpret, program);
 }
