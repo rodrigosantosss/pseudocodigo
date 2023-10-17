373591d8cc5d3a876e9a26e73926d39a6aaabc13
RodrigoS1546 <rodrigo.santos@adsglobal.pt>
2023-10-17 12:58:53 +0100

    Implemented all operations on the InterValue type

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,9 +1,9 @@
 use crate::parser::{ExprTree, Instruction, Program, Statement, Type};
 use crate::tokenizer::Token;
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Add, ops::Div, ops::Mul, ops::Sub, ops::BitAnd, ops::BitOr, ops::BitXor, ops::Not};
 
-#[derive(Clone)]
-pub enum InterVar {
+#[derive(Clone, PartialEq, PartialOrd)]
+enum InterValue {
     Integer(i64),
     Real(f64),
     Character(char),
@@ -11,7 +11,146 @@ pub enum InterVar {
     Boolean(bool),
 }
 
-impl ToString for InterVar {
+impl InterValue {
+    fn to_real(&self) -> f64 {
+        match self {
+            Self::Integer(x) => *x as f64,
+            Self::Real(x) => *x,
+            Self::Character(x) => (*x as i64) as f64,
+            Self::CharacterChain(x) => (!x.is_empty() as i64) as f64,
+            Self::Boolean(x) => (*x as i64) as f64,
+        }
+    }
+
+    fn to_integer(&self) -> i64 {
+        match self {
+            Self::Integer(x) => *x,
+            Self::Real(x) => *x as i64,
+            Self::Character(x) => *x as i64,
+            Self::CharacterChain(x) => !x.is_empty() as i64,
+            Self::Boolean(x) => *x as i64,
+        }
+    }
+
+    fn to_boolean(&self) -> bool {
+        match self {
+            Self::Integer(x) => *x != 0,
+            Self::Real(x) => *x != 0.0,
+            Self::Character(x) => *x != '\0',
+            Self::CharacterChain(x) => !x.is_empty(),
+            Self::Boolean(x) => *x,
+        }
+    }
+
+    fn pow(&self, rhs: &Self) -> Self {
+        if matches!(self, Self::Real(_)) || matches!(rhs, Self::Real(_)) {
+            Self::Real(self.to_real().powf(rhs.to_real()))
+        } else if rhs.to_integer() < 0 {
+            Self::Real(
+                self.to_real()
+                    .powi(rhs.to_integer().try_into().expect("Overflow")),
+            )
+        } else {
+            Self::Integer(
+                self.to_integer()
+                    .pow(rhs.to_integer().try_into().expect("Overflow")),
+            )
+        }
+    }
+}
+
+impl Mul for &InterValue {
+    type Output = InterValue;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if matches!(self, InterValue::Real(_)) || matches!(rhs, InterValue::Real(_)) {
+            InterValue::Real(self.to_real() * rhs.to_real())
+        } else {
+            InterValue::Integer(self.to_integer() * rhs.to_integer())
+        }
+    }
+}
+
+impl Div for &InterValue {
+    type Output = InterValue;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        InterValue::Real(self.to_real() / rhs.to_real())
+    }
+}
+
+impl Add for &InterValue {
+    type Output = InterValue;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if matches!(self, InterValue::Real(_)) || matches!(rhs, InterValue::Real(_)) {
+            InterValue::Real(self.to_real() + rhs.to_real())
+        } else {
+            InterValue::Integer(self.to_integer() + rhs.to_integer())
+        }
+    }
+}
+
+impl Sub for &InterValue {
+    type Output = InterValue;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if matches!(self, InterValue::Real(_)) || matches!(rhs, InterValue::Real(_)) {
+            InterValue::Real(self.to_real() - rhs.to_real())
+        } else {
+            InterValue::Integer(self.to_integer() - rhs.to_integer())
+        }
+    }
+}
+
+impl BitAnd for &InterValue {
+    type Output = InterValue;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        if matches!(self, InterValue::Boolean(_)) && matches!(rhs, InterValue::Boolean(_)) {
+            InterValue::Boolean(self.to_boolean() && rhs.to_boolean())
+        } else {
+            InterValue::Integer(self.to_integer() & rhs.to_integer())
+        }
+    }
+}
+
+impl BitOr for &InterValue {
+    type Output = InterValue;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        if matches!(self, InterValue::Boolean(_)) && matches!(rhs, InterValue::Boolean(_)) {
+            InterValue::Boolean(self.to_boolean() || rhs.to_boolean())
+        } else {
+            InterValue::Integer(self.to_integer() | rhs.to_integer())
+        }
+    }
+}
+
+impl BitXor for &InterValue {
+    type Output = InterValue;
+
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        if matches!(self, InterValue::Boolean(_)) && matches!(rhs, InterValue::Boolean(_)) {
+            InterValue::Boolean(self.to_boolean() ^ rhs.to_boolean())
+        } else {
+            InterValue::Integer(self.to_integer() ^ rhs.to_integer())
+        }
+    }
+}
+
+impl Not for &InterValue {
+    type Output = InterValue;
+
+    fn not(self) -> Self::Output {
+        match self {
+            InterValue::Boolean(b) => InterValue::Boolean(!*b),
+            _ => InterValue::Integer(!self.to_integer()),
+        }
+    }
+}
+
+impl ToString for InterValue {
     fn to_string(&self) -> String {
         match self {
             Self::Integer(x) => x.to_string(),
@@ -23,14 +162,17 @@ impl ToString for InterVar {
     }
 }
 
-fn evaluate_expression(expr: ExprTree, variables: &mut HashMap<Box<str>, InterVar>) -> InterVar {
+fn evaluate_expression(
+    expr: ExprTree,
+    variables: &mut HashMap<Box<str>, InterValue>,
+) -> InterValue {
     match expr.token {
         Token::Identifier(ident) => variables.get(&ident).unwrap().clone(),
-        _ => panic!()
+        _ => panic!(),
     }
 }
 
-fn interpret_statement(statement: Statement, variables: &mut HashMap<Box<str>, InterVar>) {
+fn interpret_statement(statement: Statement, variables: &mut HashMap<Box<str>, InterValue>) {
     match statement {
         Statement::SingleInstruction(Instruction::Assign(ident, expr)) => {
             let result = evaluate_expression(expr, variables);
@@ -54,11 +196,19 @@ fn interpret_statement(statement: Statement, variables: &mut HashMap<Box<str>, I
             for ident in idents {
                 std::io::stdin().read_line(&mut buffer).unwrap();
                 match &variables.get(&ident).unwrap() {
-                    InterVar::Integer(_) => variables.insert(ident, InterVar::Integer(buffer.trim().parse().unwrap())),
-                    InterVar::Real(_) => variables.insert(ident, InterVar::Real(buffer.trim().parse().unwrap())),
-                    InterVar::Character(_) => variables.insert(ident, InterVar::Character(buffer.trim().parse().unwrap())),
-                    InterVar::CharacterChain(_) => variables.insert(ident, InterVar::CharacterChain(Box::from(buffer.trim()))),
-                    InterVar::Boolean(_) => variables.insert(ident, InterVar::Boolean(buffer.trim().parse().unwrap())),
+                    InterValue::Integer(_) => {
+                        variables.insert(ident, InterValue::Integer(buffer.trim().parse().unwrap()))
+                    }
+                    InterValue::Real(_) => {
+                        variables.insert(ident, InterValue::Real(buffer.trim().parse().unwrap()))
+                    }
+                    InterValue::Character(_) => variables
+                        .insert(ident, InterValue::Character(buffer.trim().parse().unwrap())),
+                    InterValue::CharacterChain(_) => variables
+                        .insert(ident, InterValue::CharacterChain(Box::from(buffer.trim()))),
+                    InterValue::Boolean(_) => {
+                        variables.insert(ident, InterValue::Boolean(buffer.trim().parse().unwrap()))
+                    }
                 };
                 buffer.clear();
             }
@@ -68,16 +218,16 @@ fn interpret_statement(statement: Statement, variables: &mut HashMap<Box<str>, I
 }
 
 pub fn interpret(program: Program) {
-    let mut variables: HashMap<Box<str>, InterVar> = HashMap::new();
+    let mut variables: HashMap<Box<str>, InterValue> = HashMap::new();
     for (ident, var) in program.variables {
         variables.insert(
             ident,
             match var.var_type {
-                Type::Integer => InterVar::Integer(Default::default()),
-                Type::Real => InterVar::Real(Default::default()),
-                Type::Character => InterVar::Character(Default::default()),
-                Type::CharacterChain => InterVar::CharacterChain(Default::default()),
-                Type::Boolean => InterVar::Boolean(Default::default()),
+                Type::Integer => InterValue::Integer(Default::default()),
+                Type::Real => InterValue::Real(Default::default()),
+                Type::Character => InterValue::Character(Default::default()),
+                Type::CharacterChain => InterValue::CharacterChain(Default::default()),
+                Type::Boolean => InterValue::Boolean(Default::default()),
             },
         );
     }
